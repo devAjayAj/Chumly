094cc811daf6684acf6eb68a4d0d5b21b3a8b039
devAjayAj <ajayajkumar18@gmail.com>
2017-07-16 22:29:10 +0530

    Attendance Percent bar

--- app/src/main/java/com/ajad/chumly/AttendanceFragment.java
+++ app/src/main/java/com/ajad/chumly/AttendanceFragment.java
@@ -1,8 +1,10 @@
 package com.ajad.chumly;
 
 
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +26,22 @@ public class AttendanceFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_attendence, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View v, Bundle savedInstanceState){
+        CircleDisplay cd = getActivity().findViewById(R.id.circleDisplay);
+        cd.setColor(ContextCompat.getColor(getActivity(), R.color.colorPrimary));
+        cd.setAnimDuration(2000);
+        cd.setValueWidthPercent(22f);
+        cd.setTextSize(36f);
+        cd.setDrawText(true);
+        cd.setDrawInnerCircle(true);
+        cd.setFormatDigits(1);
+        cd.setTouchEnabled(false);
+        cd.setUnit("%");
+        cd.setStepSize(0.5f);
+        cd.showValue(65f, 100f, true);
     }
 
 }
